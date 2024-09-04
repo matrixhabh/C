e6f2d5262e3f20e19687bb3039912b811b24478b
matrixhabh <corpseadvanced@gmail.com>
2024-09-04 21:49:39 +0530

    Variables, Data Types

---
+++ Hello.c
@@ -0,0 +1,52 @@
+// First Code //
+
+# include <stdio.h>
+
+int main() {
+    printf("Hello World");
+    return 0;
+}
+
+// To run in terminal:- (1) gcc (given name).c >> ./a.out {mac} ./a.exe {windows}
+
+
+
+//                  "VARIABLE"
+
+// variable is the name of a memory location which stores data also, name of any variable should be meaningful 
+ 
+ /*
+
+ Variable Rules-
+ 1) Variables are case sensitive
+ 
+ i.e., int a = 30;
+       int A = 40;
+
+ 2) 1st character is always alphabet or '_'
+
+ i.e., int _age = 12; or
+       int age = 12;
+
+ 3) No comma/blank space
+  
+  i.e., int final_price = 100;
+
+4) No other symbol other than '_'
+
+also for updating values of variables (for example 2 years)
+i.e., int age = 22;
+      age =24
+
+ */
+
+// Understanding code :- int->(Data Type) age->(Memory Location) = 22->(Data Stored)
+
+// additionally:- float pi = 3.14;
+//                char Hashtag = '#';
+
+
+
+//                       "CONSTANTS"
+
+// Constants are values that don't change
